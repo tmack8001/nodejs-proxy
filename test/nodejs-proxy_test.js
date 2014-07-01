072cd0f3dd678d205757c20e97d157b3871c49fc
Trevor Mack <tmack@groupon.com>
2014-06-30 22:34:02 -0700

    grunt-ifying the project

---
+++ test/nodejs-proxy_test.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var nodejs_proxy = require('../lib/nodejs-proxy.js');
+
+/*
+  ======== A Handy Little Nodeunit Reference ========
+  https://github.com/caolan/nodeunit
+
+  Test methods:
+    test.expect(numAssertions)
+    test.done()
+  Test assertions:
+    test.ok(value, [message])
+    test.equal(actual, expected, [message])
+    test.notEqual(actual, expected, [message])
+    test.deepEqual(actual, expected, [message])
+    test.notDeepEqual(actual, expected, [message])
+    test.strictEqual(actual, expected, [message])
+    test.notStrictEqual(actual, expected, [message])
+    test.throws(block, [error], [message])
+    test.doesNotThrow(block, [error], [message])
+    test.ifError(value)
+*/
+
+/*
+  ======== A Handy NodeUnit Example ========
+exports['awesome'] = {
+  setUp: function(done) {
+    // setup here
+    done();
+  },
+  'no args': function(test) {
+    test.expect(1);
+    // tests here
+    test.equal(nodejs_proxy.awesome(), 'awesome', 'should be awesome.');
+    test.done();
+  },
+};
+*/
